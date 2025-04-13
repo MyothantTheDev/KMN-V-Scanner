e1f047e9f5a11690bde8ae7c29b70372ab6caf8d
Myo Thant <myothant.thedev@gmail.com>
2025-04-14 03:03:54 +0700

    mv(config): move into server package

--- server/config/version.py
+++ server/config/version.py
@@ -2,4 +2,6 @@
 
 __version__ = "2.1.0"
 __author__ = "Khit Minnyo"
+__contributors__ = ['Khit Minnyo', 'Myo Thant']
 __description__ = "Advanced Vulnerability Scanner with Network Discovery and Port Scanning capabilities"
+__license__ = "MIT"
